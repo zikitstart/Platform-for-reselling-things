e73b4b65d44bfe23eb62ec039c19ce7fee974654
zikit <zikit.start@yandex.ru>
2023-06-28 14:55:24 +0300

    Добавление мапперов для сущностей.

---
+++ src/main/java/ru/skypro/homework/utils/AdMapper.java
@@ -0,0 +1,50 @@
+package ru.skypro.homework.utils;
+
+import org.springframework.stereotype.Component;
+import ru.skypro.homework.dto.AdDto;
+import ru.skypro.homework.dto.CreateAdDto;
+import ru.skypro.homework.dto.FullAdDto;
+import ru.skypro.homework.model.Ad;
+import ru.skypro.homework.model.Image;
+
+import java.util.Optional;
+
+@Component
+public class AdMapper {
+
+    public AdDto adToAdDto(Ad ad) {
+        AdDto adDto = new AdDto();
+        adDto.setAuthor(ad.getUser().getIdUser());
+        adDto.setImage(Optional.ofNullable(ad.getImage())
+                .map(Image::getPath)
+                .orElse(null));
+        adDto.setPk(ad.getIdAd());
+        adDto.setPrice(ad.getPrice());
+        adDto.setTitle(ad.getTitle());
+        return adDto;
+    }
+
+    public FullAdDto adsToFullAds(Ad ad) {
+        FullAdDto fullAdDto = new FullAdDto();
+        fullAdDto.setPk(ad.getIdAd());
+        fullAdDto.setAuthorFirstName(ad.getUser().getFirstName());
+        fullAdDto.setAuthorLastName(ad.getUser().getLastName());
+        fullAdDto.setDescription(ad.getDescription());
+        fullAdDto.setEmail(ad.getUser().getUserName());
+        fullAdDto.setImage(Optional.ofNullable(ad.getImage())
+                .map(Image::getPath)
+                .orElse(null));
+        fullAdDto.setPhone(ad.getUser().getPhone());
+        fullAdDto.setPrice((ad.getPrice()));
+        fullAdDto.setTitle(ad.getTitle());
+        return fullAdDto;
+    }
+
+    public Ad CreateAdDtoToAds(CreateAdDto createAdDto) {
+        Ad ad = new Ad();
+        ad.setDescription(createAdDto.getDescription());
+        ad.setPrice(createAdDto.getPrice());
+        ad.setTitle(createAdDto.getTitle());
+        return ad;
+    }
+}
